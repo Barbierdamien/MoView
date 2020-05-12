eca71cf15fc8761e29dedd003e7ed3d36d5ff131
Spraack <spraack@gmail.com>
2020-05-12 11:50:43 +0200

    Ajout boucle for
    boucle pour avoir uniquement des films

--- script.js
+++ script.js
@@ -1,50 +1,22 @@
-  /* Récupère les donées d'un fetche */
-//   function Lecture(){
-//     fetch("http://img.omdbapi.com/?apikey=[89190b50]&")
-//        .then( response => response.json())
-//        .then(function (data){
-//            console.log(data);
-//            let liste = document.querySelector("ul");
- 
-//        //  //Filtre les altitude_mini > 900 et tri par le nombre de piste
-//            //  data.filter(elt => elt.altitude_mini > 900).sort((a, b) => a.pistes - b.pistes).forEach(elt => {
-//            let li = document.createElement("li");
-               
-//        //Fonctionne mais mauvaise manière de faire
-//            //li.innerText = elt.nom + " (" + elt.altitude_mini + " - " + elt.altitude_maxi + ")";
-               
-//        //Bonne manière de faire altgr7
-//        //Interpolation de chaine avec `` permet mettre des varaible dans le text avec ${}
-//            li.innerText = `${elt.nom} (${elt.altitude_mini}m - ${elt.altitude_maxi}m - ${elt.pistes} pistes) `
-//            liste.appendChild(li);
- 
-       
-//        })
-//        .catch(function (err) {
-//            console.log("Erreur !!!");
-//        });
-//    }
 
-
-var randomMovieArray = "https://www.omdbapi.com/?i=tt3896198&apikey=81059d9e";
-
-
+var randomMovieArray ="https://www.omdbapi.com/?i=tt3896198&apikey=81059d9e&t=";
 
 // console.log(randomMovie);
 
 function apiCall() {
 
-var randomNumber = Math.floor((Math.random() * randomMovieArray.length) +1);
+
+var randomNumber = Math.floor((Math.random() * randomMovieArray.length) + 1);
 
 var randomMovie = randomMovieArray[randomNumber];
 
+// var randomMovie = Math.floor((Math.random() * 25897) + 1);
+
+
     $.getJSON('https://www.omdbapi.com/?i=tt3896198&apikey=81059d9e&t=' + encodeURI(randomMovie)).then(function(response){
-        // var type = response.Type;
-    // var movie = "movie";
 
-    // var type = response.Type;
+    
 
-    // for (type = 0; type !== movie; type++) {
 
         var image = response.Poster;
         var titre = response.Title;
@@ -56,47 +28,67 @@ var randomMovie = randomMovieArray[randomNumber];
         var director = response.Director;
         var language = response.Language;
         var imdbrating = response.imdbRating;
+        var type = response.Type;
 
-        if(image !== "N/A"){
-            $('.poster').attr('src', image);
-        }
 
-        if(titre !== "N/A"){
-            $('.titre').text(titre);
-        }
 
-        if(plot !=="N/A"){
-            $('.resume').text(plot);
-        }
+        for (let i = 0; i < 10; i++) {
+        
+            if(type == "movie"){
 
-        if(genre !== "N/A"){
-            $('.genre').text(genre);
-        }
+                if(image !== "N/A"){
+                        $('.poster').attr('src', image);
+                    }
 
-        if(year !== "N/A"){
-            $('.annee').text(year)
-        }
+                    if(titre !== "N/A"){
+                        $('.titre').text(titre);
+                    }
 
-        if(runtime !== "N/A"){
-            $('.duree').text(runtime)
-        }
+                    if(plot !=="N/A"){
+                        $('.resume').text(plot);
+                    }
 
-        if(actors !== "N/A"){
-            $('.acteurs').text(actors)
-        }
+                    if(genre !== "N/A"){
+                        $('.genre').text(genre);
+                    }
 
-        if(director !== "N/A"){
-            $('.realisateur').text(director)
-        }
+                    if(year !== "N/A"){
+                        $('.annee').text(year)
+                    }
 
-        if(imdbrating !== "N/A"){
-            $('.noteImdb').text(imdbrating)
+                    if(runtime !== "N/A"){
+                        $('.duree').text(runtime)
+                    }
+
+                    if(actors !== "N/A"){
+                        $('.acteurs').text(actors)
+                    }
+
+                    if(director !== "N/A"){
+                        $('.realisateur').text(director)
+                    }
+
+                    if(imdbrating !== "N/A"){
+                        $('.noteImdb').text(imdbrating)
+                    }
+
+                    if(language !== "N/A"){
+                        $('.langue').text(imdbrating)
+                    }
+
+                    if(type !== "movie"){
+                        $('.type').text('hello')
+                    }else{
+                        $('.type').text(type)
+                    }
+            }else{
+                continue;
+            }
         }
-  
-    // };
        
     });
 };
+
 
 apiCall();
 
@@ -104,7 +96,10 @@ $('button').click(function(){
     apiCall();
 })
 
-// "Type":"movie"
+
+
+
+
 
 
 
